e4847be416530b526db1e6ead3e0f9d8829d9ba8
Davit Mamrikishvili <davit.mamrikishvili1@gmail.com>
2026-03-10 22:27:45 +0400

    fix: set correct baseUrl and checkout submodules in CI

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "davitmamrikishvili.github.io/saa-c03",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
